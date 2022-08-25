176e6a17468376f6228425706355b669f899edab
Acquite <acquitefx@gmail.com>
2022-08-25 15:44:35 +0100

    0.1.0

--- src/index.tsx
+++ src/index.tsx
@@ -7,43 +7,54 @@ import { Messages, Users } from 'enmity/metro/common';
 
 const Channels = getByProps('hasChannel');
 const Patcher = create('double-tap-to-edit');
-
-
+const doubleclickFunc = (e) => handler(e);
+const handler = (e) => {
+   alert(e.target.closest('li > [class^=message]'))
+}
 
 const DoubleTapToEdit: Plugin = {
    ...manifest,
 
    onStart() {
 
-      Patcher.after(Messages, 'sendMessage', (self, args, orig) => {
-         const [channelId, opts] = args;
-         // if (!opts?.content || !channelId) {
-         //    return orig.apply(self, args);
-         // }
+      try {
+			//Classes
+			this.selectedClass = getByProps("message", "selected").selected;
+			this.messagesWrapper = getByProps("empty", "messagesWrapper").messagesWrapper;
 
-         // const channel = Channels.getChannel(channelId);
-         // if (!channel) {
-         //    return orig.apply(self, args);
-         // }
+			//Reply functions
+			this.replyToMessage = getByProps("replyToMessage").replyToMessage;
+			this.getChannel = getByProps("getChannel", "getDMFromUserId").getChannel;
 
+			//Stores
+			this.MessageStore = getByProps("receiveMessage", "editMessage");
+			this.CurrentUserStore = getByProps("getCurrentUser");
 
-         // global.document.addEventListener('dblclick', () => {
-         //    Messages.startEditMessage()
-         // });
-         alert(`Channel ID 1: ${channelId}`)
-         alert(`Message ID: ${self}`)
-         alert(`Message ID: ${orig}`)
-         alert(`Message ID: ${self.id}`)
-         alert(`Message ID: ${args}`)
-         alert(`Message content: ${args[1].content}`)
+			//Settings
+			this.SwitchItem = getByProps("SwitchItem");
 
-      });
+			//Events
+			global.document.addEventListener('dblclick', doubleclickFunc);
+
+		}
+		catch (err) {
+			try {
+				console.error("Attempting to stop after starting error...", err)
+				this.stop();
+			}
+			catch (err) {
+				console.error(this.getName() + ".stop()", err);
+			}
+		}
    },
+
 
    onStop() {
-      document.removeEventListener('dblclick', () => {});
+      document.removeEventListener('dblclick', doubleclickFunc)
       Patcher.unpatchAll();
    },
+
+   
 };
 
 registerPlugin(DoubleTapToEdit);
