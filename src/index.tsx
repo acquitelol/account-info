cd598af238e7dbedf9b6622b95a45a3f3284f6cd
Acquite <acquitefx@gmail.com>
2022-08-25 03:49:52 +0100

    0.0.6

--- src/index.tsx
+++ src/index.tsx
@@ -30,10 +30,17 @@ const DoubleTapToEdit: Plugin = {
          // global.document.addEventListener('dblclick', () => {
          //    Messages.startEditMessage()
          // });
-         alert(args[0])
-         alert(args[1])
-         alert(args[2])
-         alert(args[3])
+         alert(`Alert 1: ${args[0]}`)
+         alert(`Alert 2: ${args[1]}`)
+         alert(`Alert 3: ${args[2]}`)
+         alert(`Alert 4: ${args[3]}`)
+         alert(`Alert 5: ${args[4]}`)
+
+         alert(`Self 1: ${self[0]}`)
+         alert(`Self 2: ${self[1]}`)
+         alert(`Self 3: ${self[2]}`)
+         alert(`Self 4: ${self[3]}`)
+         alert(`Self 5: ${self[4]}`)
       });
    },
 
