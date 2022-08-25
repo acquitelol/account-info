04d5d888e57650588e54374fe449793256ce315c
Acquite <acquitefx@gmail.com>
2022-08-25 03:43:39 +0100

    0.0.5 (1)

--- src/index.tsx
+++ src/index.tsx
@@ -3,27 +3,42 @@ import { React } from 'enmity/metro/common';
 import { getByProps } from 'enmity/metro';
 import { create } from 'enmity/patcher';
 import manifest from '../manifest.json';
-import { Image, Text, View } from 'enmity/components';
-import { bulk, filters } from 'enmity/metro';
+import { Constants, Messages, Users } from 'enmity/metro/common';
 
+const Channels = getByProps('hasChannel');
 const Patcher = create('double-tap-to-edit');
 
-const [
-   MessagesModule,
-] = bulk(
-   filters.byProps("sendMessage"),
-);
+
 
 const DoubleTapToEdit: Plugin = {
    ...manifest,
 
    onStart() {
-      Patcher.instead(MessagesModule, "sendMessage", (self, args, res) => {
-         alert('You are about to send a message :skull:!')
+
+      Patcher.after(Messages, 'sendMessage', (self, args, orig) => {
+         // const [channelId, opts] = args;
+         // if (!opts?.content || !channelId) {
+         //    return orig.apply(self, args);
+         // }
+
+         // const channel = Channels.getChannel(channelId);
+         // if (!channel) {
+         //    return orig.apply(self, args);
+         // }
+
+
+         // global.document.addEventListener('dblclick', () => {
+         //    Messages.startEditMessage()
+         // });
+         alert(args[0])
+         alert(args[1])
+         alert(args[2])
+         alert(args[3])
       });
    },
 
    onStop() {
+      document.removeEventListener('dblclick', () => {});
       Patcher.unpatchAll();
    },
 };
