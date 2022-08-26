7893b4a1d0aacafb0f0041b78678c3d5a1280963
Acquite <acquitefx@gmail.com>
2022-08-26 01:17:03 +0100

    smh

--- src/index.tsx
+++ src/index.tsx
@@ -11,25 +11,23 @@ const UploadsModule = getByProps("uploadLocalFiles");
 
 const DoubleTapToEdit: Plugin = {
    ...manifest,
-   patches: [],
 
-   onStart() {
- 
-     Patcher.before(MessagesModule, "sendMessage", (_, args, __) => {
-       args[1].content = args[1].content.replaceAll("media.discordapp.net", "cdn.discordapp.com")
-     });
- 
-     Patcher.before(UploadsModule, "uploadLocalFiles", (_, args, __) => {
-       args[3].content = args[3].content.replaceAll("media.discordapp.net", "cdn.discordapp.com")
-     });
-   },
- 
+  onStart() {
+
+    Patcher.before(MessagesModule, "sendMessage", (_, args, __) => {
+      args[1].content = args[1].content.replaceAll("media.discordapp.net", "cdn.discordapp.com")
+    });
+
+    Patcher.before(UploadsModule, "uploadLocalFiles", (_, args, __) => {
+      args[3].content = args[3].content.replaceAll("media.discordapp.net", "cdn.discordapp.com")
+    });
+  },
 
 
-   onStop() {
-      this.commands = [];
-      Patcher.unpatchAll();
-   },
+
+  onStop() {
+    Patcher.unpatchAll();
+  },
 
    
 };
