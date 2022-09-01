959dec3dc4cbedde41853d1b2a9f2947d0940067
Acquite <acquitefx@gmail.com>
2022-09-01 22:45:36 +0300

    test 1.2.3

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,41 +1,14 @@
 import { FormDivider, FormRow, ScrollView, FormSwitch, Form, FormSection } from 'enmity/components';
 import { SettingsStore } from 'enmity/api/settings';
-import { React, StyleSheet, Constants } from 'enmity/metro/common';
+import { React } from 'enmity/metro/common';
 
 interface SettingsProps {
    settings: SettingsStore;
 }
 
 export default ({ settings }: SettingsProps) => {
-    const styles = StyleSheet.createThemedStyleSheet({
-        container: {
-           marginLeft: 15.5,
-           marginRight: 15.5,
-           marginTop: 17.5,
-        },
-        header: {
-           color: Constants.ThemeColorMap.HEADER_SECONDARY,
-           opacity: 0.975,
-           fontFamily: Constants.Fonts.DISPLAY_BOLD,
-           textTransform: 'uppercase',
-           fontSize: 12.75,
-           letterSpacing: 0.25
-        },
-        information: {
-           borderRadius: 4,
-           backgroundColor: Constants.ThemeColorMap.BACKGROUND_TERTIARY,
-           marginTop: 10,
-           marginBottom: 10
-        },
-        item: {
-           color: Constants.ThemeColorMap.TEXT_MUTED
-        },
-        icon: {
-           color: Constants.ThemeColorMap.INTERACTIVE_NORMAL
-        }
-     });
    return <>
-    <ScrollView style={styles.container}>
+    <ScrollView>
         <FormSection title="Enable Dedicated Buttons">
             <FormRow
                 label='Profile Picture'
@@ -55,7 +28,7 @@ export default ({ settings }: SettingsProps) => {
                         onValueChange={() => settings.toggle('statusBtn')}
                     />
                 }
-            />;
+            />
         </FormSection>
     </ScrollView>
    </>
