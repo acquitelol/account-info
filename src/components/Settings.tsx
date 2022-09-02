256d0d03d246401fb403716cc455dbe8d73d020c
Acquite <acquitefx@gmail.com>
2022-09-02 12:00:41 +0300

    ver 1.4.3 build (dev)

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,95 +1,77 @@
-import {
-	FormRow,
-	FormSwitch,
-	FormInput,
-	TextInput,
-	View,
-	FormSection,
-	FormText,
-	FormLabel,
-} from "enmity/components";
-import { SettingsStore, get } from "enmity/api/settings";
-import { React, Toasts } from "enmity/metro/common";
-import { getIDByName } from "enmity/api/assets";
+import { FormDivider, FormRow, ScrollView, FormSwitch, Form, FormSection } from 'enmity/components';
+import { SettingsStore, getBoolean } from 'enmity/api/settings';
+import { getIDByName } from 'enmity/api/assets';
+import { React, Toasts } from 'enmity/metro/common';
+import { version, release} from '../../manifest.json';
 
 interface SettingsProps {
-	settings: SettingsStore;
+   settings: SettingsStore;
 }
 
 export default ({ settings }: SettingsProps) => {
-	return (
-		<View>
-			<FormSection title="Connecting Settings">
-				<FormInput
-					value={settings.get("host", "192.168.1.212")}
-					onChange={(s: string) => {
-						if (s.split(".").length >= 4) {
-							if (s.match(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)) {
-								Toasts.open({
-									content: `Valid IP address`,
-									source: getIDByName("check"),
-								});
-								settings.set("host", s);
-							} else {
-								Toasts.open({
-									content: "Invalid IP address",
-									source: getIDByName("Small"),
-								});
-							}
-						}
-					}}
-					title="Local IP Address"
-					placeholder="192.168.178.1"
-				/>
-				<FormInput
-					value={settings.get("port", "8097")}
-					onChange={(s: string) => {
-						if (s.length > 0) {
-							if (s.match(/^\d{1,5}$/)) {
-								settings.set("port", s);
-								Toasts.open({
-									content: "Valid Port",
-									source: getIDByName("check"),
-								});
-							} else {
-								Toasts.open({
-									content: "Invalid Port",
-									source: getIDByName("Small"),
-								});
-							}
-						}
-					}}
-					title="Port"
-					placeholder="8097"
-				/>
-				<FormRow
-					label="Connect"
-					trailing={FormRow.Arrow}
-					onPress={() => {
-						let code = `window.__enmityConnectToDevTools({ host: "${get(
-							"ReactDevTools",
-							"host"
-						)}", port: ${get("ReactDevTools", "port")} })`;
-						Toasts.open({
-							content: `Connecting to ${get("ReactDevTools", "host")}`,
-							source: getIDByName("check"),
-						});
-						eval(code);
-					}}
-				/>
-			</FormSection>
-			<FormSection title="Experimental Settings">
-				<FormRow
-					label="Auto Connect"
-					trailing={
-						<FormSwitch
-							value={settings.get("autoconnect", false)}
-							onValueChange={(s: boolean) => settings.set("autoconnect", s)}
-						/>
-					}
-				/>
-			</FormSection>
-			<FormRow label="If it doesn't connect, your PC's IP might be wrong use ipconfig in cmd" />
-		</View>
-	);
+   const toastTrail = getIDByName('ic_selection_checked_24px');
+   return <>
+    <ScrollView>
+        <FormSection title="Enable Account Date Information">
+            <FormRow
+                label='Time of Creation'
+                trailing={
+                    <FormSwitch
+                        value={settings.getBoolean('createBtn', true)}
+                        onValueChange={() => {
+                                settings.toggle('createBtn', true)
+                                Toasts.open({ content: 'Toggled Time of Creation', source: toastTrail });
+                            }
+                        }
+                    />
+                }
+            />
+            <FormDivider />
+            <FormRow
+                label='Time of Joining Server'
+                trailing={
+                    <FormSwitch
+                        value={settings.getBoolean('joinBtn', true)}
+                        onValueChange={() => {
+                                settings.toggle('joinBtn', true)
+                                Toasts.open({ content: 'Toggled Time of Joining Server', source: toastTrail });
+                            }
+                        }
+                    />
+                }
+            />
+        </FormSection>
+        <FormSection title="Enable Dedicated Buttons">
+            <FormRow
+                label='Profile Picture'
+                trailing={
+                    <FormSwitch
+                        value={settings.getBoolean('pfpBtn', false)}
+                        onValueChange={() => {
+                                settings.toggle('pfpBtn', false)
+                                Toasts.open({ content: 'Toggled Profile Picture Button', source: toastTrail });
+                            }
+                        }
+                    />
+                }
+            />
+            <FormDivider />
+            <FormRow
+                label='Status'
+                trailing={
+                    <FormSwitch
+                        value={settings.getBoolean('statusBtn', false)}
+                        onValueChange={() => {
+                                settings.toggle('statusBtn', false)
+                                Toasts.open({ content: 'Toggled Status Button', source: toastTrail });
+                            }
+                        }
+                    />
+                }
+            />
+        </FormSection>
+		<FormRow label={`Plugin Version: ${version}`} />
+		<FormRow label={`Release Channel: ${release}`} />
+    </ScrollView>
+   </>
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,205 @@
-import { Plugin, registerPlugin } from "enmity/managers/plugins";
-import { React, Toasts } from "enmity/metro/common";
-import Settings from "./components/Settings";
-import manifest from "../manifest.json";
-import { get } from "enmity/api/settings";
-import { getIDByName } from "enmity/api/assets";
+import { Constants, Moment, React, StyleSheet, Toasts } from 'enmity/metro/common';
+import { FormDivider, FormRow, Text, View, Pressable } from 'enmity/components';
+import { Plugin, registerPlugin } from 'enmity/managers/plugins';
+import { getIDByName } from 'enmity/api/assets';
+import { bulk, filters, getByProps } from 'enmity/metro';
+import { create } from 'enmity/patcher';
+import manifest from '../manifest.json';
+import Settings from './components/Settings';
+import { getBoolean } from 'enmity/api/settings'
 
-const ReactDevTools: Plugin = {
-	...manifest,
+const [
+   Header,
+   Members,
+   Guilds,
+   Router,
+   Clipboard,
+   AvatarHeader,
+] = bulk(
+   filters.byDisplayName('UserProfileHeader', false),
+   filters.byProps('getMember'),
+   filters.byProps('getGuild'),
+   filters.byProps('transitionToGuild'),
+   filters.byProps('setString'),
+   filters.byName('HeaderAvatar', false),
+);
 
-	onStart() {
-		if (get("ReactDevTools", "autoconnect")) {
-			let code = `window.__enmityConnectToDevTools({ host: "${get(
-				"ReactDevTools",
-				"host"
-			)}", port: ${get("ReactDevTools", "port")} })`;
-			Toasts.open({
-				content: `Connecting to ${get("ReactDevTools", "host")}`,
-				source: getIDByName("check"),
-			});
-			eval(code);
-		}
-	},
+const Patcher = create('account-info');
+const Activity = getByProps('getStatus', 'getState')
 
-	onStop() {},
+const AccountInfo: Plugin = {
+   ...manifest,
 
-	getSettingsPanel({ settings }) {
-		return <Settings settings={settings} />;
-	},
+   onStart() {
+
+      Patcher.instead(Header, 'default', (self, args, orig) => {
+         console.log(Activity)
+         let pfpBool = getBoolean("AccountInfo", 'pfpBtn', false)
+         let statusBool = getBoolean("AccountInfo", "statusBtn", false)
+         let createBool = getBoolean("AccountInfo", "createBtn", true)
+         let joinBool = getBoolean("AccountInfo", "joinBtn", true)
+
+         const [{ user, channel, type }] = args;
+
+
+         if (type !== 0) {
+            return orig.apply(self, args);
+         }
+         
+         const styles = StyleSheet.createThemedStyleSheet({
+            container: {
+               marginLeft: 15.5,
+               marginRight: 15.5,
+               marginTop: 17.5,
+            },
+            header: {
+               color: Constants.ThemeColorMap.HEADER_SECONDARY,
+               opacity: 0.975,
+               fontFamily: Constants.Fonts.DISPLAY_BOLD,
+               textTransform: 'uppercase',
+               fontSize: 12.75,
+               letterSpacing: 0.25
+            },
+            information: {
+               borderRadius: 4,
+               backgroundColor: Constants.ThemeColorMap.BACKGROUND_TERTIARY,
+               marginTop: 10,
+               marginBottom: 10
+            },
+            item: {
+               color: Constants.ThemeColorMap.TEXT_MUTED
+            },
+            icon: {
+               color: Constants.ThemeColorMap.INTERACTIVE_NORMAL
+            }
+         });
+
+         const Pfp = getIDByName('img_nitro_profile_banner');
+         const ActivityToast = getIDByName('pending-alert');
+         const ActivityForm = getIDByName('toast_copy_link');
+         const Add = getIDByName('ic_header_members_add_24px');
+         const Joined = getIDByName('ic_leave_24px');
+
+         const isGuild = channel?.guild_id;
+         const member = isGuild && Members.getMember(channel.guild_id, user.id);
+         const guild = isGuild && Guilds.getGuild(channel.guild_id);
+
+         const image = isGuild ? member?.getAvatarURL?.(false, 4096, true) || user?.getAvatarURL?.(false, 4096, true) : user?.getAvatarURL?.(false, 4096, true);
+         if (!image) return orig.apply(self, args);
+
+         const discrim = user.discriminator % 5;
+         const url = typeof image === 'number' ? `https://cdn.discordapp.com/embed/avatars/${discrim}.png` : image?.replace('.webp', '.png');
+
+         const activityContent = Activity.getActivities(user.id).find(ac => ac.type === 4)
+         
+         return <>
+            {orig.apply(self, args)}
+            <View style={styles.container}>
+               {(createBool || joinBool) ? <>
+                  <Text style={styles.header}>
+                     Account Information
+                  </Text>
+                  
+                  <View style={styles.information}>
+                     {createBool && <>
+                        <FormRow
+                           label='Created'
+                           leading={<FormRow.Icon style={styles.icon} source={Add} />}
+                           onPress={() => {
+                              Toasts.open({
+                                 content: Moment(user.createdAt).format('LLL'),
+                                 source: Add
+                              });
+                           }}
+                           trailing={() => <Text style={styles.item}>
+                              {Moment(user.createdAt).fromNow()}
+                           </Text>}
+                        />
+                     </>}
+                     {createBool && joinBool && isGuild && member && <FormDivider />}
+                     {joinBool && isGuild && member &&<>
+                        <FormRow
+                           label={`Joined ${guild?.name ?? ''}`}
+                           leading={<FormRow.Icon style={styles.icon} source={Joined} />}
+                           onPress={() => {
+                              Toasts.open({
+                                 content: Moment(member.joinedAt).format('LLL'),
+                                 source: Joined
+                              });
+                           }}
+                           trailing={() => <Text style={styles.item}>
+                              {Moment(member.joinedAt).fromNow()}
+                           </Text>}
+                        />
+                     </>}
+                  </View>
+               </> : <></>}
+               {(pfpBool || statusBool) ? <>
+                  <Text style={styles.header}>
+                     Account Assets
+                  </Text>
+                  <View style={styles.information}>
+                     {pfpBool &&
+                        <FormRow
+                           label={`View ${user.username}'s Profile Picture`}
+                           leading={<FormRow.Icon style={styles.icon} source={Pfp} />}
+                           onPress={() => {
+                              Router.openURL(url)
+                           }}
+                        />
+                     }
+                     {pfpBool && statusBool && <FormDivider />}
+                     {activityContent && statusBool && <>
+                           <FormRow
+                              label={`Copy ${user.username}'s Status`}
+                              leading={<FormRow.Icon style={styles.icon} source={ActivityForm} />}
+                              onPress={() => {
+                                 Clipboard.setString(`${activityContent.emoji.name ? `:${activityContent.emoji.name}:` : ""} ${activityContent.state ? activityContent.state : ""}`);
+                                 Toasts.open({ content: 'Copied to clipboard', source: ActivityToast });
+                              }}
+                           />
+                        </>
+                     }
+                  </View>
+               </> : <></>}
+            </View>
+         </>;
+      });
+
+      Patcher.after(AvatarHeader, 'default', (_, [{ user }], res) => {
+         let pfpBool = getBoolean("AccountInfo", 'pfpBtn', false)
+         const image = user?.getAvatarURL?.(false, 4096, true);
+         if (!image) return res;
+
+         const discrim = user.discriminator % 5;
+         const url = typeof image === 'number' ? `https://cdn.discordapp.com/embed/avatars/${discrim}.png` : image?.replace('.webp', '.png');
+
+         return pfpBool ? <>{res}</> :<Pressable onPress={() => Router.openURL(url)}>
+            {res}
+         </Pressable>;
+      })
+
+      Patcher.after(Activity, 'render', (_, [{ user }], res) => {
+         let statusBool = getBoolean("AccountInfo", "statusBtn", false)
+         const ActivityToast = getIDByName('pending-alert');
+         const activityContent = Activity.getActivities(user.id).find(ac => ac.type === 4)
+
+         return statusBool ? <>{res}</> : <Pressable onPress={() => {
+            Clipboard.setString(`${activityContent.emoji.name ? `:${activityContent.emoji.name}:` : ""} ${activityContent.state ? activityContent.state : ""}`);
+            Toasts.open({ content: 'Copied to clipboard', source: ActivityToast });
+         }}>
+            {res}
+         </Pressable>;
+      })
+   },
+
+   onStop() {
+      Patcher.unpatchAll();
+   },
+
+   getSettingsPanel({ settings }) {
+      return <Settings settings={settings} />;
+   },
 };
 
-registerPlugin(ReactDevTools);
+registerPlugin(AccountInfo);
